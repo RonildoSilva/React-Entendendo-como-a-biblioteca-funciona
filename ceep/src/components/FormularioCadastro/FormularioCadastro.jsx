8e3fa5eb206e2b837160f1285ef79624e82da583
Ronildo <ronildo.comp@gmail.com>
2020-08-27 04:32:50 -0300

    lidando com eventos e binds

--- ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,12 +3,23 @@ import { Component } from 'react';
 
 class FormularioCadastro extends Component {
 
+    constructor(){
+        super();
+        this.titulo = '';
+    }
+
+    handleMudancaTitulo(evento){
+        this.titulo = evento.target.value;
+    }
+
     render(){
         return(            
             <div className="row">
             <form>
                 <div className="form-group">
-                    <input type="text" placeholder="Titulo"/>
+                    <input type="text" placeholder="Titulo"
+                    onChange={this.handleMudancaTitulo.bind(this)}
+                    />
                 </div>
                 <div className="form-group">
                     <textarea className="form-control" placeholder="Nota" 
