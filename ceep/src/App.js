e78e753eadabfac4641d31884442f11cf9b03b90
Ronildo <ronildo.comp@gmail.com>
2020-09-01 02:10:41 -0300

    Criando fonte de dados

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -3,37 +3,15 @@ import ListaDeCategorias from './components/ListaDeCategorias/ListaDeCategorias'
 import ListaDeNotas from './components/ListaDeNotas/ListaDeNotas'
 import FormularioCadastro from './components/FormularioCadastro/FormularioCadastro'
 
+import Categorias from './dados/Categorias'
+import ArrayDeNotas from './dados/Notas'
+
 class App extends Component {
   
   constructor(){
     super();
-    this.state = {
-      notas:[],
-      categorias:['cat1', 'cat2'],
-    };
-  }
-
-  criarNota(titulo, texto, categoria){
-    const novaNota = {titulo, texto, categoria};
-    const novoArrayNotas = [...this.state.notas, novaNota]
-    const novoEstado = {
-      notas: novoArrayNotas
-    }
-    this.setState(novoEstado)
-  }
-
-  deletarNota(index){
-    let arrayNotas = this.state.notas;
-    arrayNotas.splice(index, 1);
-    this.setState({notas:arrayNotas});
-    console.log('dd');
-  }
-
-  adicionarCategoria(nomeCategoria){
-    const novoArrayCategorias = [...this.state.categorias, nomeCategoria]
-    const novoEstado = {...this.state, categorias:novoArrayCategorias};
-    this.setState(novoEstado);
-
+    this.categorias = new Categorias();
+    this.notas = new ArrayDeNotas();
   }
 
   render(){
@@ -43,18 +21,18 @@ class App extends Component {
         <div class="row">
         
         <FormularioCadastro 
-          categorias={this.state.categorias}
-          criarNota={this.criarNota.bind(this)}
+          categorias={this.categorias.categorias}
+          criarNota={this.notas.criarNota}
         />
         <div class="col-sm-8">
           <ListaDeCategorias 
-            adicionarCategoria={this.adicionarCategoria.bind(this)}
-            categorias={this.state.categorias}
+            adicionarCategoria={this.categorias.adicionarCategoria}
+            categorias={this.categorias.categorias}
           />
 
           <ListaDeNotas 
-            apagarNota={this.deletarNota.bind(this)}
-            notas={this.state.notas}
+            apagarNota={this.notas.apagarNota}
+            notas={this.notas.notas}
           />
         </div>       
 
